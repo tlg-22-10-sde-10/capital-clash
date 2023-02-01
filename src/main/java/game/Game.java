183a4e9ff8eed1480d96ef07eccc7d8662262bf4
sw <aaeuio@gmail.com>
2023-02-01 10:28:47 -0800

    added something

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -1,11 +1,30 @@
 package game;
 
+import java.util.Scanner;
+
 public class Game {
 
     private final int GAME_DAYS = 5;
 
 
+    public void play() {
+
+        int selection=1;
+        do{
+            displayASCII();
+            System.out.println("Would you like to take the challenge?");
+            System.out.println("1 for yes, 2 for no.");
+            Scanner userResponse=new Scanner(System.in);
+            selection=userResponse.nextInt();
+        //System.out.println(selection);
 
 
+        }while(selection==1);
 
+    }
+
+    public static void displayASCII(){
+
+        System.out.println("Welcome to Capital Clash.");
+    }
 }
