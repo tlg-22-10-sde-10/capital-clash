3bb4e16801f11f8f0d14c1cdb31606772dc9793f
Rupesh Ghimire <rupesh.ghimire@tlgcohort.com>
2023-02-01 12:14:24 -0500

    setup classes, AB#252

---
+++ src/main/java/Account.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+
+public class Account {
+
+    private String accountName;
+    private double cashBalance;
+    private double stockValueBalance;
+    private ArrayList<Stock> listOfPlayerStocks;
+
+
+}
---
+++ src/main/java/GameClient.java
@@ -0,0 +1,7 @@
+public class GameClient {
+
+
+    public static void main(String[] args) {
+
+    }
+}
