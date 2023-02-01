6b69f10a864dad445ea23ac4b097c5ae684cd119
sw <aaeuio@gmail.com>
2023-02-01 15:37:19 -0800

    AB#552

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -8,38 +8,71 @@ import java.util.Scanner;
 public class Game {
 
     private final int GAME_DAYS = 5;
-    UserInterface ui  = new UserInterface();
 
-    public Game() {
-    }
 
     public void gameOn() {
+        UserInterface.displayASCII();
+        //refactor and use a user interface class
+        System.out.println("Would you like to take the challenge?");
+        System.out.println("1: Yes \n2: No");
+        Scanner userResponse = new Scanner(System.in);
+        int selection = userResponse.nextInt();
 
-        ui.displayASCII();
-        ui.gameBegin();
-        String userInput = ui.userInput();
-        if (userInput.equalsIgnoreCase("1")) {
+        if (selection == 1) {
             play();
 
         } else {
             //exit out of game
-            System.out.println("Thank you for participating!");
-
+            System.out.println("Bummer");
         }
-    }
 
+    }
 
     private void play() {
-        ui.displayGameInfo();
-        while(true) {
-            String userInput = ui.mainMenu();
-            if(userInput.equalsIgnoreCase("1")) {
-                ui.getNewsfeed();
-            } else if(userInput.equalsIgnoreCase("2")) {
-                ui.tradingRoomMenu();
-            } else {
-                break;
-            }
+        UserInterface.displayGameInfo();
+        //UserInterface.tradingRoomMenu();
+
+        int day = 0;
+        Scanner stdInt = new Scanner(System.in);
+
+        while (day < GAME_DAYS) {
+
+            System.out.println("It is day " + day + ".");
+
+            int mainMenuSelection = 0;
+            do {
+                UserInterface.mainMenu();
+                mainMenuSelection = stdInt.nextInt();
+                switch (mainMenuSelection) {
+                    case 1://going to trading room
+                        System.out.println("You are in trading room.");
+                        break;
+                    case 2:
+                        System.out.println("You are in news room.");
+                        break;
+                    case 3:
+                        System.out.println("You are done for the day. The game will move to the next day.");
+                        break;
+                    default:
+                        System.out.println("Invalid choice.");
+                }
+
+
+            } while (mainMenuSelection != 3);
+
+            day++;
+
         }
+
+        //Which room would you like to go to
+        //--1.News feed room -- news for the day
+        //--2.Trade room -- place order for the day
+        //--3.Go Home -- move on to the next day
+
+
+        System.out.println("Let's play");
+
     }
+
+
 }
