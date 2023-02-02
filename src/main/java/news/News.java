a4ad9d1b8e8e98a2e86c1e013f7f19f95f2c5b51
sw <aaeuio@gmail.com>
2023-02-02 13:45:29 -0800

    fixed news not changing problem

--- src/main/java/news/News.java
+++ src/main/java/news/News.java
@@ -9,7 +9,7 @@ public class News {
             2,"Boeing to set up a new 737 MAX assembly line in Everett",
             3,"Pfizer Bids Adieu to Covid Boom Years",
             4,"Facebook and Instagram’s parent said it is paring back spending plans and focusing on its social media roots",
-            5,"JPMorgan says CEO Jamie Dimon is iaving emergency heart surgery",
+            5,"JPMorgan says CEO Jamie Dimon is having emergency heart surgery",
             6,"Powell: Inflation Has Eased Somewhat but Remains Elevated",
             7,"U.S. has intel that Russian commanders have orders to proceed with Finland invasion",
             8,"Justice Dept. Sues to Block $13 Billion Deal by UnitedHealth Group",
