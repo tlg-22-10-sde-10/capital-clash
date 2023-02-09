1dcd05429b37a3d7f9f4eb345919b0c0acee90dc
sw <aaeuio@gmail.com>
2023-02-09 11:09:12 -0800

    AB#707

--- src/main/java/random/RandomNumberForNews.java
+++ src/main/java/random/RandomNumberForNews.java
@@ -1,10 +1,24 @@
 package random;
 
+import java.util.HashSet;
 import java.util.Random;
+import java.util.Set;
 
 public class RandomNumberForNews {
+
+    private static Set<Integer> shownNews= new HashSet<>();
+
     public static int getRandomNumber() {
         Random random = new Random();
-        return random.nextInt(10) + 1;
+        int number=random.nextInt(10) + 1;
+
+        while(shownNews.contains(number)){
+            number=random.nextInt(10) + 1;
+        }
+        //for testing to verify no news shown twice
+        //System.out.println(shownNews);
+
+        shownNews.add(number);
+        return number;
     }
 }
